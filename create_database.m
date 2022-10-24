94f32e765a3dba12111b00e1ac5ae579a94091e4
aledevehat <110026302+aledevehat@users.noreply.github.com>
2022-10-24 10:19:41 +0200

    Update create_database.m

--- create_database.m
+++ create_database.m
@@ -53,3 +53,19 @@ Set_finding:=procedure(A,p,l,~list)
     end if;
   
 end procedure;      
+
+Create_database:=function()
+
+  primes := [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149];
+  Database_list := [* *];
+  for p in primes do
+    l_primes:= [l: l in primes| l lt p];
+    for l in l_primes do
+        for A in [1 .. p] do
+          procedure(A,p,l,~Database_list);
+        end for;
+    end for;
+  end for;
+  return Databse_list ;
+end function;
+
