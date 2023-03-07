81a87ee50af48f92b4ee6133e2e3afda41f2bb50
Gustavo Banegas <gsouzaba@qti.qualcomm.com>
2023-03-07 18:02:18 +0100

    fix

--- n_kvelu/ctool.py
+++ n_kvelu/ctool.py
@@ -6,14 +6,14 @@ class OpCount(object):
     nr_inv = 0
 
     field_op = {}
-    
+
     __instance = None
 
     def __new__(cls, *args, **kwargs):
         if not OpCount.__instance:
             OpCount.__instance = object.__new__(cls)
         return OpCount.__instance
-   
+
     @staticmethod
     def op(operation, field=None):
         if field == None:
@@ -21,14 +21,21 @@ class OpCount(object):
         try:
             OpCount.field_op[field][operation] += 1
         except:
-            OpCount.field_op[field] ={}
-            OpCount.field_op[field][operation] = 1
-    
+            try:
+                t = OpCount.field_op[field]
+                try:
+                    a = OpCount.field_op[field][operation]
+                except:
+                    OpCount.field_op[field][operation] = 1
+            except:
+                OpCount.field_op[field] = {}
+                OpCount.field_op[field][operation] = 1
+
     @staticmethod
     def print_results():
         print("-- \t Printing number of Operations \t --")
         for k in OpCount.field_op:
             print("-- \t Operations in k = {} \t --".format(k))
             for op in OpCount.field_op[k]:
-                print("\t op: {} - nr: {}".format(op, OpCount.field_op[k][op])) 
+                print("\t op: {} - nr: {}".format(op, OpCount.field_op[k][op]))
         print("---------------------------------------------")
