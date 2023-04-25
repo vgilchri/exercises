01d8e55b1d640bddfbfaae400a4852fe23a5e10e
Gustavo S Banegas <gustavo@cryptme.in>
2023-04-25 09:37:08 +0200

    add parser from Magma to params

--- parser/parse.py
+++ parser/parse.py
@@ -1,12 +1,37 @@
-import re
 
+import re
+#A, p, l, k, EC, j-inv, ratio, point, image
 def rreplace(s, old, new):
     return (s[::-1].replace(old[::-1],new[::-1], 1))[::-1]
+
+def get_parameters(st):
+    res = re.sub(r'EllipticCurve(.*?)', '', st)
+    broken_lines = res.split(",")
+    A = int(broken_lines[0])
+    p = int(broken_lines[1])
+    l = int(broken_lines[2])
+    k = int(broken_lines[3])
+    j_inv = int(broken_lines[9])
+    ratio = int(broken_lines[10])
+    param = (A, p, l, k, j_inv, ratio)
+    return param
 
 # Open a file: file
 file = open('gustavo',mode='r')
 
 # read all lines at once
 all_of_it = file.read().replace("[*", "", 1)
-tmp = all_of_it[:-2] 
-print(tmp)
+all_of_it = all_of_it.replace(" ", "")
+
+tmp = all_of_it[:-2]
+pat = r'\[\*|\*\]'
+match = re.split(pat,tmp)
+parameters = []
+for e in match:
+    new_str = e.replace("\n", "").replace("\\", "")
+    if len(new_str) > 2:
+        param = get_parameters(new_str)
+        parameters.append(param)
+
+for param in parameters:
+    print(param)
